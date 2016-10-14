8169276b74d6720cc5fd27d87a9f3fe9a9b70c43
ASM-SE <andersonmarcolino@gmail.com>
2016-10-13 22:02:28 -0300

    angularjs and controllers - initUser function

--- NodeJS/jobs/web/js/controllers/user-controller.js
+++ NodeJS/jobs/web/js/controllers/user-controller.js
@@ -5,11 +5,11 @@
   controller.$inject = ['$scope']; //injentando dependencias segundo o jhonpapa https://github.com/johnpapa/angular-styleguide
 
   function controller($scope){
-
-    $scope.users = [];
+    $scope.initUsers = () =>{ //funcção que incializa a lista -- elemento ul na index.html
+      $scope.users = [];
+    }
 
     $scope.save = function(){
-
       $scope.users.push($scope.user);
       $scope.user = {};
     }
