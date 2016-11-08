a0fc0ac3f5bcacb5d9383ffc5727f87b1c2d8147
ASM-SE <andersonmarcolino@gmail.com>
2016-11-08 21:38:43 -0200

    menu

---
+++ NodeJS/jobs/web/js/config/icon-config.js
@@ -0,0 +1,20 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('jobs').config(config);
+
+  config.$inject = ['$mdIconProvider']; //provedor de icones
+
+  function config($mdIconProvider) {
+
+    $mdIconProvider
+           .icon('navigation:menu', '/img/icons/ic_menu.svg')
+           .icon('social:people', '/img/icons/ic_people.svg')
+           .icon('social:people_pin', '/img/icons/ic_person_pin.svg')
+           .icon('admin:business', '/img/icons/ic_business.svg')
+           .icon('admin:jobs', '/img/icons/ic_work.svg')
+           .icon('admin:logout', '/img/icons/ic_exit.svg');
+
+  }
+
+})(window.angular);
--- NodeJS/jobs/web/js/controllers/menu-controller.js
+++ NodeJS/jobs/web/js/controllers/menu-controller.js
@@ -6,15 +6,45 @@
 
   angular.module('jobs').controller('MenuController', controller);
 
-  controller.$inject = ['$mdSidenav']; //injentando dependencias segundo o jhonpapa https://github.com/johnpapa/angular-styleguide
+  controller.$inject = ['$mdSidenav','$location']; //injentando dependencias segundo o jhonpapa https://github.com/johnpapa/angular-styleguide
 
-  function controller($mdSidenav){
+  function controller($mdSidenav, $location){
     const vm = this; //view model - como definido por john papa
     const MENU_ID = 'left';
+    vm.sections = [];
+
+    vm.sections.push({
+      title: 'Administration',
+      itens: [{ icon: 'social:people', //img/icons/ic_people_black_24px.svg
+                title: 'Users',
+                path: '/users' },
+              { icon: 'admin:business',
+                title: 'Companies',
+                path: '/companies' },
+              { icon: 'admin:jobs',
+                title: 'Jobs',
+                path: '/jobs' }]
+    });
+
+    vm.sections.push({
+        title: 'Personal',
+        itens: [{ icon: 'social:people_pin',
+                  title: 'My Profile',
+                  path: '/profile' },
+                { icon: 'admin:logout',
+                  title: 'Logout',
+                  path: '/login' }]
+    });
+
 
     vm.toggleMenu = function(){
       return $mdSidenav(MENU_ID).toggle();
     };
+
+    vm.goTo = (item) =>{
+      $location.path(item.path);
+      vm.toggleMenu();
+    }
 
 
 }
