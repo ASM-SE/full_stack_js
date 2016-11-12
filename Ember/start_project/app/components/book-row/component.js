a716463b6990e8d71a7ae6402e7ec4758c91f25c
ASM-SE <andersonmarcolino@gmail.com>
2016-11-12 16:24:54 -0200

    ember book-row component #2

--- Ember/start_project/app/components/book-row/component.js
+++ Ember/start_project/app/components/book-row/component.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'tr'
+  tagName: 'tr',
+  classNameBindings: ['divisible:success'],
+
+  //number: 5,
+  divisible: Ember.computed('model.id', 'number', function(){
+    return this.get('model.id') % this.get('number') === 0;
+  }),
+
+  didInsertElement(){
+    this.$().hide().fadeIn();
+  }
 });
