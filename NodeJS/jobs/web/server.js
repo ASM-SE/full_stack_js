8316b7801cf3b4d6c32ca081e59dd85e551d1e0d
ASM-SE <andersonmarcolino@gmail.com>
2016-11-24 21:30:10 -0200

    configuring compression

--- NodeJS/jobs/web/server.js
+++ NodeJS/jobs/web/server.js
@@ -3,10 +3,16 @@
 
 //Habilitando cache no servidor
 const express = require('express');
+const compression = require('compression');
 const app = express();
 const isProduction = process.env.NODE_ENV == 'production';
 const cacheOption = isProduction ? { maxAge: 'id'} : {};
 const port = process.env.PORT || 4000;
+
+//Para não mostrar que o express foi utilizado
+app.disable('x-powered-by');
+
+app.use(compression());
 
 app.use('/', express.static(isProduction ? 'dist' : 'src', cacheOption));
 app.listen(port, () => console.log(`Listen on ${port}.`););
